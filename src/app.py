9200bfb9500ae1fc6cad5439b4a15d7de92a15ed
Joan Gallego Frances <123939267+joangallego@users.noreply.github.com>
2025-03-14 10:17:40 +0000

    Subiendo el proyecto al nuevo repositorio

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False } 
+    ]
+
+
+
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    return jsonify(todos)
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    todos.append(dict(request_body))
+    return jsonify(todos)
+
+@app.route("/todos/<int:position>", methods=["DELETE"])
+def delete_todo(position):
+    if 0 <= position < len(todos):
+        del todos[position]
+    return jsonify(todos)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
